862fe0b2a612e297150a0aaaa4d3cae660d600f8
kuwabara414 <kuwabara@colla-tech.co.jp>
2021-03-11 18:30:12 +0900

    課題提出 PHP - パラメーターの受取（GETとPOST）

---
+++ parameter/parameterForm.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ParameterForm</title>
+</head>
+<body>
+    <form action="http://localhost/kuwabaraphp/parameter/parameterPost.php" method="post">
+    str1：<input type="text" name="str1" /><br>
+    str3：<input type="text" name="str3" /><br>
+    str2：<input type="text" name="str2" /><br>
+    <input type="submit" value="表示">
+</body>
+<body>
+</body>
+</html>
---
+++ parameter/parameterGet.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ParameterGet</title>
+    <link rel="stylesheet" href="akafutoji.css">
+</head>
+<body>
+    <p><?php echo('str1は、'.'<span class="akafutoji">'.$_GET['str1'].'</span>'.'です。'); ?></p>
+    <p><?php echo('str2は、'.'<span class="akafutoji">'.$_GET['str2'].'</span>'.'です。'); ?></p>
+    <p><?php echo('str3は、'.'<span class="akafutoji">'.$_GET['str3'].'</span>'.'です。'); ?></p>
+</body>
+<body>
+</body>
+</html>
