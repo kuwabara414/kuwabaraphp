7c8a5a7065f5f69f6a1ec180e5df1669ab7c5e9f
kuwabara414 <kuwabara@colla-tech.co.jp>
2021-03-12 15:00:04 +0900

    課題提出 PHP - 四則演算

---
+++ taskCalc/taskCalc.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>taskCalc</title>
+</head>
+<body>
+    <p><?php echo("(1 + 3) × 5 = " . (1 + 3) * 5); ?></p>
+    <p><?php echo("22 ÷ 3 = " . round(22 / 3, 3)); ?></p>
+    <p><?php echo("22 ÷ 3 = " . ceil(22 / 3)); ?></p>
+    <p><?php echo("22 ÷ 3 = " . floor(22 / 3)); ?></p>
+    <p><?php echo("36 ÷ 5 = " . round(36 / 5, 2)); ?></p>
+</body>
+<body>
+</body>
+</html>
