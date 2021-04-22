7ae1e5f3a12bd5b99e7d2baf384b023ecf3d38a0
kuwabara414 <kuwabara@colla-tech.co.jp>
2021-04-22 19:28:04 +0900

    課題提出 PHP - データ削除

---
+++ db/delete.php
@@ -0,0 +1,15 @@
+<DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>delete</title>
+</head>
+<body>
+    課題1
+    <p><?php
+        echo('DELETE FROM heritage WHERE id in (2, 3);');
+    ?></p>
+</body>
+</html>
